ac9252d112a2f0782d6b221c0db8c1cbf5593453
Alok Yadav <yalok6321@gmail.com>
2024-11-11 08:49:00 +0530

    add jwtPayload type

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -3,41 +3,47 @@ import { errorResponse } from "../utils/response";
 import jwt from "jsonwebtoken";
 import User from "../models/user";
 
+interface JwtPayload {
+    _id: string;
+    email: string;
+}
+
 const isLoggedIn = async (req: Request, res: Response, next: NextFunction) => {
-	try {
-		const token =
-			req.cookies.token ||
-			req.header("Authorization")?.replace("Bearer ", "");
+    try {
+        const token =
+            req.cookies.token ||
+            req.header("Authorization")?.replace("Bearer ", "");
 
-		if (!token) {
-			errorResponse(res, "Access denied, no token provided", 401);
+        if (!token) {
+            errorResponse(res, "Access denied, no token provided", 401);
+            return;
+        }
 
-			return;
-		}
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET!
+        ) as JwtPayload;
 
-		const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+        const existingUser = await User.findById(decoded._id);
 
-		const existingUser = await User.findById((decoded as any)._id);
+        if (!existingUser) {
+            errorResponse(res, "User not found", 404);
+            return;
+        }
 
-		if (!existingUser) {
-			errorResponse(res, "User not found", 404);
+        const user = {
+            _id: existingUser._id as string,
+            name: existingUser.name,
+            email: existingUser.email,
+            role: existingUser.role
+        };
 
-			return;
-		}
-
-		const user = {
-			_id: existingUser._id as string,
-			name: existingUser.name,
-			email: existingUser.email,
-			role: existingUser.role,
-		};
-
-		req.user = user;
-		next();
-	} catch (error) {
-		console.log(error);
-		errorResponse(res, "Something went wrong", 500);
-	}
+        req.user = user;
+        next();
+    } catch (error) {
+        console.log(error);
+        errorResponse(res, "Something went wrong", 500);
+    }
 };
 
 export default isLoggedIn;
