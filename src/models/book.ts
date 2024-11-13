5ae6e81a1c5a3e59a0fa5bae49d1056b16f72db2
Alok Yadav <yalok6321@gmail.com>
2024-11-13 17:02:49 +0530

    update book and user model

--- src/models/book.ts
+++ src/models/book.ts
@@ -1,60 +1,69 @@
 import { Document, model, Schema } from "mongoose";
 
 export interface IBook extends Document {
-	title: string;
-	author: string;
-	description: string;
-	price: number;
-	stock: number;
-	ratings: number;
-	category: Schema.Types.ObjectId;
-	reviews: Schema.Types.ObjectId[];
-	createdAt: Date;
-	updatedAt: Date;
-
-	_id: Schema.Types.ObjectId;
+    _id: Schema.Types.ObjectId;
+    title: string;
+    author: string;
+    description: string;
+    price: number;
+    stock: number;
+    ratings: number;
+    coverImage: string;
+    file: string;
+    category: Schema.Types.ObjectId;
+    reviews: Schema.Types.ObjectId[];
+    createdAt: Date;
+    updatedAt: Date;
 }
 
 const BookSchema = new Schema<IBook>(
-	{
-		title: {
-			type: String,
-			required: true,
-		},
-		author: {
-			type: String,
-			required: true,
-		},
-		description: {
-			type: String,
-			required: true,
-		},
-		price: {
-			type: Number,
-			required: true,
-		},
-		stock: {
-			type: Number,
-			required: true,
-		},
-		ratings: {
-			type: Number,
-			default: 0,
-		},
-		category: {
-			type: Schema.Types.ObjectId,
-			ref: "Category",
-		},
-		reviews: [
-			{
-				type: Schema.Types.ObjectId,
-				ref: "Review",
-			},
-		],
-	},
-	{
-		timestamps: true,
-	}
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        author: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        stock: {
+            type: Number,
+            required: true
+        },
+        ratings: {
+            type: Number,
+            default: 0
+        },
+        coverImage: {
+            type: String,
+            required: true
+        },
+        file: {
+            type: String,
+            required: true
+        },
+        category: {
+            type: Schema.Types.ObjectId,
+            ref: "Category"
+        },
+        reviews: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Review"
+            }
+        ]
+    },
+    {
+        timestamps: true
+    }
 );
 
 const Book = model<IBook>("Book", BookSchema);
